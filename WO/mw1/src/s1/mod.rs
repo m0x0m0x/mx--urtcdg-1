28484522c5496587fad68bdfd8ae03bfcce82897
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-17 09:24:49 +0000

    🫦

--- WO/mw1/src/s1/mod.rs
+++ WO/mw1/src/s1/mod.rs
@@ -6,6 +6,10 @@ use crate::utils::print_with_synthwave_gradient;
 use yansi::Paint;
 
 pub fn s1main() {
+    func1();
+}
+
+fn func1() {
     print_with_synthwave_gradient("Section 1".to_string());
     println!("{}", "Booty".red());
 }
