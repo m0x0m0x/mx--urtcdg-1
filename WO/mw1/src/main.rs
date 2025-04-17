1a19a1ee020e339fdc15eca2b1b80380b4a613e7
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-17 08:59:26 +0000

    🫦

--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -1,3 +1,10 @@
+/*
+Main entry point for the Rust application.
+*/
+
+mod utils;
+use utils::print_with_synthwave_gradient;
+
 fn main() {
-    println!("Hello, world!");
+    print_with_synthwave_gradient("Welcome to Synthwave CLI".to_string());
 }
