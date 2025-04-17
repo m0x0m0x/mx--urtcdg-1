9e8a7e6bfff1c1c71a8338679201c3eb2171ffa0
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-17 17:35:13 +0000

    🫦

--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -5,9 +5,9 @@ Main entry point for the Rust application.
 mod mytest;
 mod s1;
 mod utils;
-use mytest::mstrt;
+use mytest::mstrt::mstrt_main;
 // use s1::s1main;
 
 fn main() {
-    mstrt::mstrt_main();
+    mstrt_main();
 }
