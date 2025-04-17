9d4a4bf852c3e5fed6c3b0557e76b758e3c2ff0b
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-17 12:30:46 +0000

    🫦

--- WO/mw1/src/s1/mod.rs
+++ WO/mw1/src/s1/mod.rs
@@ -26,35 +26,36 @@ struct Deck {
 
 // Inherent Implemenation
 impl Deck {
-    fn new() -> Self {}
+    fn new() -> Self {
+        // list of suits
+        let suits = ["SS", "HH", "AA", "DD"];
+        // List of values
+        let values = ["Ace", "Two", "Three"];
+
+        // Empty vector of cards
+        let mut cards = vec![];
+
+        // Example of reassignment
+        // cards = vec![];
+
+        // Double nested for loops - to generate combinations
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        let deck = Deck { cards };
+        return deck;
+    }
 }
 
 fn s1_fn() {
     // Printing the header text
     pswg("Section 1 - Structs and Vectors".to_string());
 
-    // defining a vector of strings which are called bindings
-    //  Another way of declaring vector - Vec::new()
+    let deck = Deck::new();
 
-    // list of suits
-    let suits = ["SS", "HH", "AA", "DD"];
-    // List of values
-    let values = ["Ace", "Two", "Three"];
-
-    // Empty vector of cards
-    let mut cards = vec![];
-
-    // Example of reassignment
-    // cards = vec![];
-
-    // Double nested for loops - to generate combinations
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
-        }
-    }
-
-    let deck = Deck { cards };
     println!("Deck: {:#?}", deck.green());
 }
