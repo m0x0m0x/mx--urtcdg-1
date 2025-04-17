9f8cd5d8399725729a684c18b06b119e98d90aac
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-17 09:23:07 +0000

    🫦

--- WO/mw1/src/s1/mod.rs
+++ WO/mw1/src/s1/mod.rs
@@ -3,8 +3,9 @@ Sectin 1 Work here
 */
 
 use crate::utils::print_with_synthwave_gradient;
+use yansi::Paint;
 
 pub fn s1main() {
     print_with_synthwave_gradient("Section 1".to_string());
-    println!("This is Section 1");
+    println!("{}", "Booty".red());
 }
