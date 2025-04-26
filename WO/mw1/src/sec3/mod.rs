d58faad7300f3eeb21ac366e81d513c3cd885ace
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-26 16:39:58 +0000

    🫦

--- WO/mw1/src/sec3/mod.rs
+++ WO/mw1/src/sec3/mod.rs
@@ -3,11 +3,14 @@ Note the mod keyword will have the file which contains the function
 .
 ├── mod.rs
 └── w1.rs
+└── bank1.rs
 - Note the file structure
 - If there was w2.rs , Then the declaratin would be
 pub mod w2
 - Then in main.rs , you will call the function within w2.rs
 - For example the function inside w1.rs is w1_sec3_main, which is bein called in main.rs for execution
+- bank1
 */
 
+pub mod bank1;
 pub mod w1;
