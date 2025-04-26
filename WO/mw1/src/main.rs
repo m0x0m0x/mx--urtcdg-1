d342d313ef26db20419c8f154f56b0eef5b0df29
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-26 16:45:03 +0000

    🫦

--- WO/mw1/src/main.rs
+++ WO/mw1/src/main.rs
@@ -4,8 +4,8 @@ Main entry point for the Rust application.
 
 mod sec3;
 mod utils;
-use sec3::w1::w1_sec3_main;
+use sec3::bank1::bank1_main;
 
 fn main() {
-    w1_sec3_main();
+    bank1_main();
 }
--- WO/mw1/src/sec3/bank1.rs
+++ WO/mw1/src/sec3/bank1.rs
@@ -6,7 +6,12 @@ use crate::utils::{header, pswg};
 
 //////// /// Main function call ////////////
 pub fn bank1_main() {
-    header("Section 3 - Bank Project");
-    pswg("Bank Project".to_string());
-    // bank1_main();
+    sb1();
+}
+
+////// Sub functions here //////////
+
+fn sb1() {
+    pswg("Sub Bank 1".to_string());
+    header("Sub Function test")
 }
