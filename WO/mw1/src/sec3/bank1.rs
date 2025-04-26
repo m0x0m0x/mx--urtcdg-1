d8050fe7584fd2f9e7fdd41cf79e947464bef5c6
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-04-26 16:43:21 +0000

    🫦

---
+++ WO/mw1/src/sec3/bank1.rs
@@ -0,0 +1,12 @@
+/*
+Section 3 - Bank Project work
+*/
+
+use crate::utils::{header, pswg};
+
+//////// /// Main function call ////////////
+pub fn bank1_main() {
+    header("Section 3 - Bank Project");
+    pswg("Bank Project".to_string());
+    // bank1_main();
+}
